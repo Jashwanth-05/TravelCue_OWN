f2a48099cabda25f53dfb2d34d71549fef5cf943
Jashwanth-05 <jashwanthj510@gmail.com>
2024-09-08 20:34:44 +0530

    Webpabe Updated

---
+++ Home.js
@@ -0,0 +1,5 @@
+const button =document.getElementById("start");
+function changetoapp(){
+    window.location.href="/App.html"
+}
+button.onclick =changetoapp;
--- scripts.js
+++ scripts.js
@@ -57,30 +57,6 @@ function CloseMap()
         console.log("No tracking to stop");
     }
     }
-
-// function getCoordinates(address){
-//     var apiKey = 'AkZmA6inQ8X33Y7xyC7h937_FX52x5PCi6w0GZ_NS4o-S79PasmZnsXphgYbo5Hv';
-//     var encodedAddress = encodeURIComponent(address);
-//     var url = `https://dev.virtualearth.net/REST/v1/Locations?q=${encodedAddress}&key=${apiKey}`;
-//     $.ajax({
-//         url: url,
-//         type: "GET",
-//         dataType: "json",
-//         success: function (data) {
-//             console.log(data);
-//             if (data && data.resourceSets && data.resourceSets.length > 0 && data.resourceSets[0].resources.length > 0) {
-//                 var location = data.resourceSets[0].resources[0].point.coordinates;
-//                 endLat= location[0];
-//                 endLon = location[1];
-//             } else {
-//                 console.log('No results found');
-//             }
-//         },
-//         error: function (error) {
-//             console.log(`Error ${error}`);
-//         }
-//     });
-// }
 function calculateDistance() {
     var apiKey = 'AkZmA6inQ8X33Y7xyC7h937_FX52x5PCi6w0GZ_NS4o-S79PasmZnsXphgYbo5Hv';
     var url = `https://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=${startLat},${startLon}&wp.1=${endLat},${endLon}&key=${apiKey}`;
@@ -100,8 +76,7 @@ function calculateDistance() {
                 playSound();
                 alert('The destination is less than 500 meters away!');
                 setTimeout(stopAlertSound,600);
-            }else{
-                alert(`Distance: ${distance} km Travel Time: ${duration / 60} minutes`)
+                StopTracking;
             }
         } else {
                 console.log('No results found');
